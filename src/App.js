fb1151d8c8cb06a63c2021008368e3bf734b661b
Calin <calinoiu.alexandru@agilefreaks.com>
2017-12-17 14:14:47 +0200

    Higher level components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+const HOC = (InnerComponent) => class extends React.Component {
     constructor() {
         super();
-        this.state = { items: [] }
+        this.state = { count: 0 };
+    }
+
+    update() {
+        this.setState({ count: this.state.count + 1 })
     }
 
     componentWillMount() {
-        fetch('https://swapi.co/api/people/?format=json')
-            .then(response => response.json())
-            .then( ({results: items}) => this.setState({items}) )
-    }
-
-    filter(e) {
-        this.setState({filter: e.target.value});
+        console.log('Will mount')
     }
 
     render() {
-        let items = this.state.items;
-        if (this.state.filter) {
-            items = items.filter(item => item.name.toLowerCase().includes(this.state.filter.toLowerCase()))
-        }
+        return (
+            <InnerComponent
+                {...this.props}
+                {...this.state} 
+                update={this.update.bind(this)}/>
+        )
+    }
+}
+
+class App extends React.Component {
+    render() {
         return (
             <div>
-                <input onChange={this.filter.bind(this)} />
-                {items.map(item => <Person  key={item.name} person={item} />)}
+                <Button>button</Button>
+                <hr />
+                <LabelHOC>label</LabelHOC>
             </div>
         )
     }
 }
 
-const Person = (props) => <h4>{props.person.name}</h4>
+const Button = HOC((props) => <button onClick={props.update}>{props.children} - {props.count}</button>)
 
-App.defaultProps = { val: 0 }
+class Label extends React.Component {
+    componentWillMount() {
+        console.log('Label will mount');
+    }
+
+    render() {
+        return (
+            <label onMouseMove={this.props.update}>{this.props.children} - {this.props.count}</label>
+        )
+    }
+}
+
+const LabelHOC = HOC(Label)
 
 export default App
