c803d58d6af47edebf2e88fa792ff219ce38c439
Calin <calinoiu.alexandru@agilefreaks.com>
2017-12-17 18:10:02 +0200

    Iterating over component children

--- src/App.js
+++ src/App.js
@@ -1,46 +1,24 @@
 import React from 'react';
-import './App.css'
 
 class App extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            input: '/* add your jsx here */',
-            output: '',
-            err: '',
-        }
-    }
-
-    update(e) {
-        const code = e.target.value;
-
-        try {
-            this.setState({
-                output: window.Babel.transform(code, { presets: ['es2015', 'react'] }).code,
-                err: '',
-            })
-        }
-        catch (err) {
-            this.setState({err: err.message});
-        }
-    }
-
     render() {
         return (
-            <div>
-                <header>{this.state.err}</header>
-                <div className="container">
-                    <textarea
-                        onChange={this.update.bind(this)}
-                        defaultValue={this.state.input}
-                    />
-                    <pre>
-                        {this.state.output}
-                    </pre>
-                </div>
-            </div>
+            <Parent>
+                <div className="childA" />
+                {/* <div className="childB" />                 */}
+            </Parent>
         )
+    }
+}
+
+class Parent extends React.Component {
+    render() {
+        // const items = React.Children.map(this.props.children, child => child)
+        // const items = React.Children.toArray(this.props.children)
+        // console.log(items)
+        // React.Children.forEach(this.props.children, child => console.log(child))
+        React.Children.only(this.props.children);
+        return null;
     }
 }
 
